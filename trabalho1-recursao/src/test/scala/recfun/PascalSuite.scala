a90f4caa25e167d31ef59b2dbb70056d4b0602d0
unknown <renan.nascimento@NBVIX1240.ISH.LOCAL>
2015-09-10 13:43:12 -0300

    Triângulo de Pascal, trabalho1, realizado.

--- trabalho1-recursao/src/test/scala/recfun/PascalSuite.scala
+++ trabalho1-recursao/src/test/scala/recfun/PascalSuite.scala
@@ -11,7 +11,22 @@ class PascalSuite extends FunSuite {
   import Main.pascal
 
   test("pascal: col=0,row=2") {
+    assert(pascal(0,2) === 1)
+  }
+  test("pascal: col=0,row=0") {
     assert(pascal(0,0) === 1)
+  }
+  test("pascal: col=5,row=5") {
+    assert(pascal(5,5) === 1)
+  }
+  test("pascal: col=1,row=3") {
+    assert(pascal(1,3) === 3)
+  }
+  test("pascal: col=4,row=2") {
+    assert(pascal(2,4) === 6)
+  }
+  test("pascal: col=5,row=10") {
+    assert(pascal(5,10) === 252)
   }
 
   // ==================================================================
